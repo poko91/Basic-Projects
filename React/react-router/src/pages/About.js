b39807510d252ff59d5081a49cb04e2e7b2408d3
Poorna Kamat <kamatpoorna@gmail.com>
2023-09-27 17:08:43 +0900

    initial commit

---
+++ React/react-router/src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="about">
+      <h2>About Us</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique quae tempore dolor, amet ab cupiditate sit qui accusantium nihil molestias? Blanditiis, totam. Eligendi adipisci animi, atque repellendus saepe nihil magni.</p>
+    </div>
+  );
+}
---
+++ React/react-router/src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="Home">
+      <h2>Welcome</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, dignissimos sed at officia quia voluptas dolor fuga laboriosam aliquam id sint ratione perferendis hic, ipsum consequuntur! Totam vel accusantium ut!</p>
+    </div>
+  );
+}
